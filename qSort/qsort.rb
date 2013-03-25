51e5c9e663834d0c0cf6d2403b52af5b06d9e247
Robbie Perlstein <rperlster@gmail.com>
2013-03-25 16:14:13 -0600

    Second Ruby program
    Reminds me of Python!

---
+++ qSort/qsort.rb
@@ -0,0 +1,64 @@
+# Robbie Perlstein
+# Mar 25, 2013
+# Ruby second program
+# quicksort - sorting a 1000 element array
+
+# made a class for no reason
+class QSort
+
+	private
+	def swap( qArr, val1, val2 )
+		qArr[val1], qArr[val2] = qArr[val2], qArr[val1]
+	end
+	
+	def partition( qArr, left, right, pivot )
+		pivotVal = qArr[pivot]
+		swap( qArr, pivot, right )
+		
+		swapIndex = left
+		for i in (left..(right-1))
+			if qArr[i] < pivotVal 
+				swap( qArr, i, swapIndex )
+				swapIndex += 1
+			end
+		end
+		swap( qArr, swapIndex, right )
+		return swapIndex
+	end
+	
+	def sort( qArr, left, right )
+		if left < right then
+			pivot = left + (right-left)/2
+			pivot.floor
+			pivot = partition( qArr, left, right, pivot )
+			sort( qArr, left, pivot - 1 )
+			sort( qArr, pivot + 1, right )
+		end
+	end
+	
+	public
+	# Sort an entire array. Removes nil entries. 
+	# All elements must be the same type. 
+	# Return sort time
+	def qSort( qArr )
+		qArr.delete_if{ |x| x == nil }
+		t1 = Time.now
+		sort( qArr, 0, qArr.size-1 )
+		t2 = Time.now
+		return (t2 - t1)/1000
+	end
+end
+
+
+a = Array.new
+for i in 0...1000
+	a.push( Random.rand( 1..1000 ) )
+end
+puts( a.size )
+print( "\n\nQuick sort Ruby demo. Sort a random array with 1000 elements.\nPress ENTER to continue...\n" )
+gets
+print( "Initial array: ", a, "\n\n" )
+qSorter = QSort.new
+time = qSorter.qSort( a )
+print( "Sorted array: ", a )
+print( "\nSorted in ", "%5.10f" % time, " seconds." )
